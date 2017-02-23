2a8ce19e49c954cd5fe290b37b966da2a013a17d
LeenAlShenibr <Distraught63@gmail.com>
2017-02-23 14:22:35 -0500

    Cleaned up crypto

--- reference_implementation/tumblebit/crypto.py
+++ reference_implementation/tumblebit/crypto.py
@@ -1,9 +1,66 @@
+# -*- coding: utf-8 -*-
+
+"""
+tumblebit.crypto
+~~~~~~~~~
+
+"""
+
+import hmac
 import ctypes
 import hashlib
-import hmac
+import logging
 
-from tumblebit import _ssl, ChaCha_ctx
+from tumblebit import _ssl, ChaCha_ctx, BNToBin
 
+########################################################
+## Random
+########################################################
+
+def get_random(bits, mod=None):
+    """
+    Returns a random byte string of size `bits`/8 bytes.
+
+    Args:
+        bits (int): The number of bits the random string should have.
+        mod (:obj:`ctypes.c_void_p`, optional): A pointer to a BN instance
+    Returns:
+        A byte strings of length `bits`/8 or None if an error occured
+        If mod is set the random byte string will have a value < mod
+    """
+    ctx = _ssl.BN_CTX_new()
+    _ssl.BN_CTX_start(ctx)
+    r = _ssl.BN_CTX_get(ctx)
+    ret = _ssl.BN_CTX_get(ctx)
+
+    if mod:
+        if _ssl.BN_rand_range(r, mod) == 0:
+            logging.debug("get_random: failed to generate random number")
+            return None
+
+        while _ssl.BN_gcd(ret, r, mod, ctx) != 1:
+            logging.debug("R is not a relative prime")
+            if _ssl.BN_rand_range(r, mod) == 0:
+                logging.debug("get_random: failed to generate random number")
+                return None
+
+    else:
+        if _ssl.BN_rand(r, bits, 0, 1) == 0:
+            logging.debug("get_random: failed to generate random number")
+            return None
+
+    rand = BNToBin(r, bits//8)
+
+    _ssl.BN_free(r)
+    _ssl.BN_free(ret)
+    _ssl.BN_CTX_end(ctx)
+    _ssl.BN_CTX_free(ctx)
+
+    return rand
+
+########################################################
+## Hash & MAC Functions
+########################################################
 
 def ripemd160(msg):
     h = hashlib.new('ripemd160')
@@ -23,12 +80,26 @@ def sha512(msg):
     h.update(msg)
     return h.digest()
 
-def hmac_sha256(msg, key):
+def hmac_sha256(key, msg):
     h = hmac.new(key, msg, hashlib.sha256)
     return h.digest()
 
 
-def xor_bytes(a, b) :
+########################################################
+## Encryption Functions
+########################################################
+
+
+def xor_bytes(a, b):
+    """ XOR's the bytes of `a` and `b`
+
+    All arguments should be byte strings.
+
+    Returns:
+        Result as a byte string or None in failure.
+    """
+    if len(a) != len(b):
+        return None
     return bytes(x ^ y for x, y in zip(a, b))
 
 
